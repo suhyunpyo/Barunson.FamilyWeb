3e72f255314e65a80a77a9c694a5a2baa2e13fcd
suhyun.pyo <suhyun.pyo@barunn.net>
2023-12-08 10:07:04 +0900

    모두 삭제

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Barunson.FamilyWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
